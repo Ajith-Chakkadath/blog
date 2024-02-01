5205e16fb6090936dfbc3efb52477b84d3f541fa
Ajith Chakkadath <130172872+Ajith-Chakkadath@users.noreply.github.com>
2024-02-01 15:03:09 +0530

    2nd commit
    connected to database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
+const dotenv = require('dotenv')
 const express = require('express')
+const mongoose = require('mongoose')
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_connect).then(() => {console.log('Mongodb is connected')}).catch((err)=>{
+    console.log(err)
+})
 
 
 const app = express()
